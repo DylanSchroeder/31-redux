37646424f7c0dca4c3049679a3e6751bab733f52
Dylan Schroeder <schrodac@gmail.com>
2018-09-26 21:39:36 -0500

    can now log actions and view with redux dev tools in browser

--- src/lib/store.js
+++ src/lib/store.js
@@ -1,6 +1,13 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import reducer from '../reducers';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import logger from './logger';
 
 export default () =>
   createStore(reducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+    composeWithDevTools(
+      applyMiddleware(
+        logger
+      )
+    )
+  );
