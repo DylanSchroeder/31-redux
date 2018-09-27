44e9ca1dad137c456e061c9ae6c62f9647fddb71
Dylan Schroeder <schrodac@gmail.com>
2018-09-26 21:38:07 -0500

    add error middleware and actions

--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -3,45 +3,48 @@ import { connect } from 'react-redux';
 import CategoryForm from '../category-form/category-form';
 import CategoryItem from '../category-item/category-item';
 import * as actions from '../../actions/category-actions';
+import * as errorActions from '../../actions/error-actions';
 
 class DashboardContainer extends Component {
-  constructor(props) {
-    super(props);
+  // constructor(props) {
+  //   super(props);
 
-    this.state = {
-      error: null,
-    };
-  }
+  //   this.state = {
+  //     error: null,
+  //   };
+  // }
 
   handleCreateCategory = (category) => {
     if (!category.name) {
-      this.setState({ error: 'need to have a name'});
-      return;
+      
+      return this.props.errorValidation('need to have a name');
     }
+    this.props.errorClear();
 
     this.props.categoryCreate(category);
-    this.setState(({
-      error: null,
-    }));
+    // this.setState(({
+    //   error: null,
+    // }));
   }
 
   handleUpdateCategory = (category) => {
     if(!category.name) {
-      return this.setState({ error: ' need to have a name' });
+      return this.props.errorValidation('need to have a name');
     }
+    this.props.errorClear();
 
     this.props.categoryUpdate(category);
-    this.setState(({
-      error: null,
-    }));
+    // this.setState(({
+    //   error: null,
+    // }));
   }
 
   render() {
     return (
       <React.Fragment>
         <h2>This is the Dashboard</h2>
-        {this.state.error &&
-          <div className='error'>{this.state.error}</div>}
+        {this.props.error &&
+          <div className='error'>{this.props.error}</div>}
 
         <CategoryForm 
           handleComplete = {this.handleCreateCategory}
@@ -59,13 +62,17 @@ class DashboardContainer extends Component {
 
 const mapStateToProps = (state) => {
   return {
+    error: state.error,
     categories: state.categories,
   };
 };
 
 const mapDispatchToProps = (dispatch) => ({
+  errorClear: () => dispatch(errorActions.errorClear()),
+  errorValidation: (error) => dispatch(errorActions.errorValidation(error)),
   categoryCreate: (category) => dispatch(actions.categoryCreate(category)),
   categoryUpdate: (category) => dispatch(actions.categoryUpdate(category)),
+
 });
 
 let connector = connect(mapStateToProps, mapDispatchToProps);
