7e2dfddb491ff14b79cc4cf847f73440245e03b0
Dylan Schroeder <schrodac@gmail.com>
2018-10-08 09:28:34 -0500

    add update/edit modal for categories

--- src/components/category-item/category-item.js
+++ src/components/category-item/category-item.js
@@ -4,31 +4,49 @@ import CategoryForm from '../category-form/category-form';
 import ExpenseForm from '../expense-form/expense-form';
 import ExpenseItem from '../expense-item';
 import * as expenseActions from '../../actions/expense-actions';
+import Modal from '../modal/modal';
 
-
-const defaultState = {
-  _id: null,
-  name: '',
-  budget: 0,
-};
+// const defaultState = {
+//   _id: null,
+//   name: '',
+//   budget: 0,
+// };
 
 class CategoryItem extends Component {
   constructor(props) {
     super(props);
-    this.state = props.category || defaultState;
+    this.state = { showEdit: false }/*props.category || defaultState*/;
   }
 
   updateCategory=(category) => {
     this.props.handleUpdate(category);
   }
 
+  showEditModal = () => {
+    this.setState({
+      showEdit: true,
+    });
+  }
+
+  hideEditModal = () => {
+    this.setState({
+      showEdit: false,
+    });
+  }
+
+
   render() {
     const { category, expenses } = this.props;
     return (
       <li>
         {category.name} : ${category.budget}
-        <CategoryForm category = {category} handleComplete = {this.updateCategory}/>
+        <button onClick={this.showEditModal}>Update</button>
+        <Modal show={this.state.showEdit} handleClose={this.hideEditModal}>
+          <CategoryForm category = {category} handleComplete = {this.updateCategory}/>
+        </Modal>
+
         <ExpenseForm category={category} handleComplete={this.props.expenseCreate}/>
+        
         {expenses.map((expense, i) => (
           <ExpenseItem key ={i} handleUpdate={this.props.expenseUpdate} expense={expense} category={expense.categoryId}/>
         ))}
